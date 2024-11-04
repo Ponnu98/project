d2c4c62222c47a5f5eb9b4000bb477028d383dfe
Sruthysuresh123 <shylajasuresh914@gmail.com>
2024-11-04 11:05:08 +0530

    first commit

---
+++ src/Home/Home.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import './Home.css';
+
+
+
+
+ function Home() {
+    return (
+        <>
+    
+    <div class="liji">    
+    <h1>
+     <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT6lyTMX25HNU60ijGkdwPjtQMC171CB3hZKw&s"width="40px" height="40px" class="img-thumbnail" alt="..."></img>uizard</h1></div><br/>
+     <div class="ponnu">
+     <h1>Why Your Startup Needs</h1>
+     </div><br/>
+     <div class="sruthy">
+     <h1>a Landing Page</h1>
+     </div>
+      
+
+
+    
+           </>
+    )
+}
+export default Home
---
+++ src/main.jsx
@@ -0,0 +1,14 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+// Import our custom CSS
+import '../src/scss/style.scss'
+
+// Import all of Bootstrap's JS
+import * as bootstrap from 'bootstrap'
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
